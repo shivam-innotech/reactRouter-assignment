4995b61937bb90f5addd1830bb75acd2b6704838
shivam maurya <shivam@innotech.in>
2022-04-19 10:36:01 +0530

    create component,pages and use bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import "./App.css"
+import { Routes,Route } from 'react-router-dom'
+import AboutUs from './pages/AboutUs'
+import ContactUs from './pages/ContactUs'
+import Messages from './pages/Messages'
+import Navbar from './components/Navbar'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path='/aboutUs'element={<AboutUs/>}/>
+      <Route path='/contactUs'element={<ContactUs/>}/>
+      <Route path='/messages'element={<Messages/>}/>
+
+    </Routes>
+   
+    </>
   )
 }
 
